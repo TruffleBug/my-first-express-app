928d2449f68b0e3f5917592aa1a8003dab5c4a33
TruffleBug <lisa.ho1022@gmail.com>
2025-04-17 16:27:40 -0600

    Revert "Add delete route"
    This reverts commit 59d02f78b062e290db5b73b5a2d63938fd43aeaa.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController');
 indexRouter
     .get('/', userController.getUsernames)
     .get('/search', userController.usersSearch)
+    .post('/', userController.deleteAllUsers)
 
 module.exports = indexRouter;
